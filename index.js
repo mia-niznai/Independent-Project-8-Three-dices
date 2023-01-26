f8906410a62fe7b94b6d546757ad224e90e82069
mia-niznai <116540572+mia-niznai@users.noreply.github.com>
2023-01-26 17:08:41 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const select3 = document.getElementById('select3');
+const select6 = document.getElementById('select6');
+const select9 = document.getElementById('select9');
+const dice3 = document.getElementById('dice3');
+const dice6 = document.getElementById('dice6');
+const dice9 = document.getElementById('dice9');
